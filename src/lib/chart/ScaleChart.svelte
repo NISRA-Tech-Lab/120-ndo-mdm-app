9d0d485efa204c10946acf7960535cd919b4c578
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-05-29 12:13:32 +0100

    simplify svg

--- src/lib/chart/ScaleChart.svelte
+++ src/lib/chart/ScaleChart.svelte
@@ -1,118 +1,48 @@
 <script>
   export let value;
 
+ import { scaleColours } from '../layout/ScaleColours.svelte';
+
   $: x_position = (value / 850 * -151.90294) - 1.3229166;
 </script>
 
 
 <svg
-   width="151.90294mm"
-   height="10.583334mm"
    viewBox="0 0 151.90294 10.583334"
-   version="1.1"
-   id="svg1"
-   xml:space="preserve"
-   inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)"
-   sodipodi:docname="colour-scale.svg"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:svg="http://www.w3.org/2000/svg"><sodipodi:namedview
-     id="namedview1"
-     pagecolor="#ffffff"
-     bordercolor="#000000"
-     borderopacity="0.25"
-     inkscape:showpageshadow="2"
-     inkscape:pageopacity="0.0"
-     inkscape:pagecheckerboard="true"
-     inkscape:deskcolor="#d1d1d1"
-     inkscape:document-units="mm"
-     inkscape:zoom="1.0449705"
-     inkscape:cx="438.28987"
-     inkscape:cy="-7.6557182"
-     inkscape:window-width="1718"
-     inkscape:window-height="1360"
-     inkscape:window-x="3633"
-     inkscape:window-y="0"
-     inkscape:window-maximized="0"
-     inkscape:current-layer="svg1" /><defs
-     id="defs1" /><g
+   xmlns="http://www.w3.org/2000/svg">
+   <g
      id="g10"
-     inkscape:label="Colour bars"
-     transform="translate(-33.784712,-93.201939)"><rect
-       style="fill:#071f59;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect1"
-       width="16.710997"
-       height="6.5831203"
-       x="34.181587"
-       y="95.202042" /><rect
-       style="fill:#182b5f;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect2"
-       width="16.710997"
-       height="6.5831203"
-       x="49.114719"
-       y="95.202042" /><rect
-       style="fill:#283865;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect3"
-       width="16.710997"
-       height="6.5831203"
-       x="64.047852"
-       y="95.202042" /><rect
-       style="fill:#3a446a;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect4"
-       width="16.710997"
-       height="6.5831203"
-       x="78.98098"
-       y="95.202042" /><rect
-       style="fill:#4c5171;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect5"
-       width="16.710997"
-       height="6.5831203"
-       x="93.914116"
-       y="95.202042" /><rect
-       style="fill:#5e5e77;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect6"
-       width="16.710997"
-       height="6.5831203"
-       x="108.84724"
-       y="95.202042" /><rect
-       style="fill:#706a7d;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect7"
-       width="16.710997"
-       height="6.5831203"
-       x="123.78038"
-       y="95.202042" /><rect
-       style="fill:#837683;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect8"
-       width="16.710997"
-       height="6.5831203"
-       x="138.71352"
-       y="95.202042" /><rect
-       style="fill:#958489;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect9"
-       width="16.710997"
-       height="6.5831203"
-       x="153.64664"
-       y="95.202042" /><rect
-       style="fill:#a8908f;fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
-       id="rect10"
-       width="16.710997"
-       height="6.5831203"
-       x="168.57977"
-       y="95.202042" /></g><rect
-     x="{x_position}"
-     y="0.2645835"
-     width="1.3229166"
-     height="10.054167"
-     stroke="#042433"
-     stroke-width="0.529167"
-     stroke-miterlimit="8"
-     fill="#f8f8f8"
-     transform="scale(-1,1)"
-     id="marker"
-     inkscape:label="marker" /></svg>
+     transform="translate(-33.784712,-93.201939)">
+      {#each scaleColours as color, i}
+        <rect
+          style="fill:{color};fill-opacity:1;stroke:#ffffff;stroke-width:0.79375"
+          id="rect{i + 1}"
+          width="16.710997"
+          height="6.5831203"
+          x="{34.181587 + (i * 14.933132)}"
+          y="95.202042" />
+      {/each}
+    </g>
+     <rect
+      x="{x_position}"
+      y="0.2645835"
+      width="1.3229166"
+      height="10.054167"
+      stroke="#042433"
+      stroke-width="0.529167"
+      stroke-miterlimit="8"
+      fill="#f8f8f8"
+      transform="scale(-1,1)"
+      id="marker"
+      inkscape:label="marker" />
+     </svg>
 
 
 <style>
-
+  svg {
+    width: 100%;
+    height: auto;
+    max-width: 600px;
+    display: block;
+  }
 </style>
